7403675073482bfec11930b82615f8cad1fd3404
Maksim Volkau <max.volkov@gmail.com>
2017-09-01 12:21:06 +0300

    added: NetStandard 2.0 target and tests on NetCore 2.0 changed: Pumped version to 1.4

--- build.bat
+++ build.bat
@@ -4,8 +4,11 @@ dotnet restore
 
 dotnet build
 
-dotnet test ".\test\FastExpressionCompiler.UnitTests"
-dotnet test ".\test\FastExpressionCompiler.IssueTests"
+dotnet test -f netcoreapp1.1 ".\test\FastExpressionCompiler.UnitTests"
+dotnet test -f netcoreapp1.1 ".\test\FastExpressionCompiler.IssueTests"
+
+dotnet test -f netcoreapp2.0 ".\test\FastExpressionCompiler.UnitTests"
+dotnet test -f netcoreapp2.0 ".\test\FastExpressionCompiler.IssueTests"
 
 dotnet pack ".\src\FastExpressionCompiler" -c Release -o "./dist"
 
