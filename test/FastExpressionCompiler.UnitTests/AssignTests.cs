92130933fbebee3e343e9e9dd649c4f008a9a1b9
Péter Csajtai <peter.csajtai@outlook.com>
2017-09-01 22:31:56 +0200

    Added member assignment, unit tests

--- test/FastExpressionCompiler.UnitTests/AssignTests.cs
+++ test/FastExpressionCompiler.UnitTests/AssignTests.cs
@@ -20,5 +20,41 @@ namespace FastExpressionCompiler.UnitTests
             Assert.IsNotNull(f);
             Assert.AreEqual("aaa", f("ignored"));
         }
+
+        [Test]
+        public void Member_test_prop()
+        {
+            var a = new Test();
+            var expr = Lambda<Func<int>>(
+                Assign(Property(Constant(a), "Prop"), 
+                Constant(5)));
+
+            var f = expr.CompileFast(true);
+
+            Assert.IsNotNull(f);
+            Assert.AreEqual(5, f());
+            Assert.AreEqual(5, a.Prop);
+        }
+
+        [Test]
+        public void Member_test_field()
+        {
+            var a = new Test();
+            var expr = Lambda<Func<int>>(
+                Assign(Field(Constant(a), "Field"),
+                Constant(5)));
+
+            var f = expr.CompileFast(true);
+
+            Assert.IsNotNull(f);
+            Assert.AreEqual(5, f());
+            Assert.AreEqual(5, a.Field);
+        }
+
+        public class Test
+        {
+            public int Prop { get; set; }
+            public int Field;
+        }
     }
 }
