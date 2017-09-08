01406acf0589343462e8f82890496f264d315509
Maksim Volkau <max.volkov@gmail.com>
2017-09-08 12:48:14 +0000

    changed: Projects and Tests to target NetCore 2.0; fixed: travis build.sh; added: travis badge

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-dotnet restore && dotnet build && dotnet test
+dotnet restore && dotnet build -f netcoreapp2.0 -c Release && dotnet test ./test/FastExpressionCompiler.UnitTests && dotnet test ./test/FastExpressionCompiler.IssueTests
 
 #dotnet pack ".\src\FastExpressionCompiler" -c Release -o "./dist"
