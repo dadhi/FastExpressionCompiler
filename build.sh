decd14b1d0e4d3b72b04d4d85e529a3b59e4efb7
Peter Csajtai <peter.csajtai@logmein.com>
2017-09-08 16:54:10 +0200

    Fixed test paths

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ dotnet restore
 if test "$TRAVIS_OS_NAME" != "osx";
 then 
 dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp1.1;
-dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp1.1;
+dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp1.1;
 fi
 
 dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp2.0;
-dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp2.0;
+dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp2.0;
