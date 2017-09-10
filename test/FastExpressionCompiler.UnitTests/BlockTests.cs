1bb2647a0099fbd59f7ea2e05ea0889c54d2f40b
Péter Csajtai <peter.csajtai@outlook.com>
2017-09-10 23:33:03 +0200

    Fixed build error

--- test/FastExpressionCompiler.UnitTests/BlockTests.cs
+++ test/FastExpressionCompiler.UnitTests/BlockTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Reflection;
 using NUnit.Framework;
 using static System.Linq.Expressions.Expression;
 
