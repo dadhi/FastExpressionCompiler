a62a5baffbbce8764fdf0af235a6c9571c38c269
Péter Csajtai <peter.csajtai@outlook.com>
2017-09-10 23:58:03 +0200

    Added more unit tests

--- test/FastExpressionCompiler.UnitTests/BlockTests.cs
+++ test/FastExpressionCompiler.UnitTests/BlockTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Reflection;
 using NUnit.Framework;
 using static System.Linq.Expressions.Expression;
@@ -15,14 +13,10 @@ namespace FastExpressionCompiler.UnitTests
         {
             var variable = Variable(typeof(int));
             var variable2 = Variable(typeof(int));
-
-            var expressions = new List<Expression>
-            {
-                Assign(variable, Constant(5)),
-                Assign(variable2, Constant(6))
-            };
-
-            var block = Block(new[] { variable, variable2 }, expressions);
+            
+            var block = Block(new[] { variable, variable2 }, 
+                Assign(variable, Constant(5)), 
+                Assign(variable2, Constant(6)));
 
             var lambda = Lambda<Func<int>>(block);
 
@@ -37,14 +31,10 @@ namespace FastExpressionCompiler.UnitTests
         {
             var variable = Variable(typeof(int));
             var variable2 = Variable(typeof(int));
-
-            var expressions = new List<Expression>
-            {
+            
+            var block = Block(new[] { variable, variable2 }, 
                 Assign(variable, Constant(5)),
-                Invoke(Lambda(Assign(variable2, Constant(6))))
-            };
-
-            var block = Block(new[] { variable, variable2 }, expressions);
+                Invoke(Lambda(Assign(variable2, Constant(6)))));
 
             var lambda = Lambda<Func<int>>(block);
 
@@ -55,17 +45,51 @@ namespace FastExpressionCompiler.UnitTests
         }
 
         [Test]
+        public void Block_local_variable_assignment_with_lambda_with_param()
+        {
+            var variable = Variable(typeof(int));
+            var variable2 = Variable(typeof(int));
+            var param = Parameter(typeof(int));
+            
+            var block = Block(new[] { variable }, 
+                Assign(variable, Constant(5)),
+                Invoke(Lambda(Block(new[] { variable2 }, 
+                Assign(variable2, param)))));
+
+            var lambda = Lambda<Func<int, int>>(block, param);
+
+            var fastCompiled = ExpressionCompiler.TryCompile<Func<int, int>>(lambda);
+
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(8, fastCompiled(8));
+        }
+
+        [Test]
+        public void Block_local_variable_assignment_with_lambda_with_param_override()
+        {
+            var variable = Variable(typeof(int));
+            var param = Parameter(typeof(int));
+            
+            var block = Block(new[] { variable }, 
+                Assign(variable, Constant(5)),
+                Invoke(Lambda(Block(Assign(param, variable)))));
+
+            var lambda = Lambda<Func<int, int>>(block, param);
+
+            var fastCompiled = ExpressionCompiler.TryCompile<Func<int, int>>(lambda);
+
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(5, fastCompiled(8));
+        }
+
+        [Test]
         public void Block_local_variable_assignment_return_with_variable()
         {
             var variable = Variable(typeof(int));
-
-            var expressions = new List<Expression>
-            {
+            
+            var block = Block(new[] { variable }, 
                 Assign(variable, Constant(5)),
-                variable
-            };
-
-            var block = Block(new[] { variable }, expressions);
+                variable);
 
             var lambda = Lambda<Func<int>>(block);
 
@@ -79,14 +103,10 @@ namespace FastExpressionCompiler.UnitTests
         public void Block_local_variable_assignment_with_member_init()
         {
             var variable = Variable(typeof(A));
-
-            var expressions = new List<Expression>
-            {
+            
+            var block = Block(new[] { variable }, 
                 Assign(variable, MemberInit(New(typeof(A).GetConstructor(Type.EmptyTypes)), Bind(typeof(A).GetProperty("K"), Constant(5)))),
-                Property(variable, typeof(A).GetProperty("K"))
-            };
-
-            var block = Block(new[] { variable }, expressions);
+                Property(variable, typeof(A).GetProperty("K")));
 
             var lambda = Lambda<Func<int>>(block);
 
