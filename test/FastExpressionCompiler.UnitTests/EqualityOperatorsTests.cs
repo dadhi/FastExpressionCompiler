20c520b88e15b05afffb7f4e7e5cf5cb36779849
Maksim Volkau <max.volkov@gmail.com>
2017-11-21 19:13:39 +0000

    added: Test for #50

--- test/FastExpressionCompiler.UnitTests/EqualityOperatorsTests.cs
+++ test/FastExpressionCompiler.UnitTests/EqualityOperatorsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using NUnit.Framework;
 
@@ -51,6 +52,42 @@ namespace FastExpressionCompiler.UnitTests
             var f = e.CompileFast(true);
 
             Assert.IsTrue(f());
+        }
+
+        [Test]
+        public void Complex_expression_with_DateTime_Strings_and_Int()
+        {
+            var dtNow = DateTime.Now;
+            var version = new Entity { DateType = 1, AvailableDate = dtNow };
+            var startTime = "b"; // a <= b
+            var endTime = "x"; // y >= x
+            var timeSliceId = "42";
+
+            Expression<Func<Entity, bool>> e = w =>
+                w.DateType == version.DateType &&
+                w.AvailableDate == version.AvailableDate &&
+                string.Compare(w.StartTime, startTime) <= 0 &&
+                string.Compare(w.EndTime, endTime) >= 0 &&
+                w.Id != timeSliceId;
+
+            var f = e.CompileFast(true);
+
+
+            var tested = new Entity
+            {
+                DateType = 1, AvailableDate = dtNow,
+                StartTime = "a", EndTime = "y"
+            };
+            Assert.IsTrue(f(tested));
+        }
+
+        public class Entity
+        {
+            public string Id { get; set; }
+            public int  DateType { get; set; }
+            public string StartTime { get; set; }
+            public string EndTime { get; set; }
+            public DateTime AvailableDate { get; set; }
         }
     }
 }
