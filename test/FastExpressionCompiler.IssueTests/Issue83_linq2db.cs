7397c4c86e8ad7dcb5cb8fb99e8776e5dd88f8d0
MaceWindu <dmitry.github@lukashenko.com>
2018-08-18 14:31:01 +0200

    add first test

---
+++ test/FastExpressionCompiler.IssueTests/Issue83_linq2db.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace FastExpressionCompiler.IssueTests
+{
+    [TestFixture]
+    public class Issue83_linq2db
+    {
+        [Test]
+        public void String_to_number_conversion_using_convert_with_method()
+        {
+            var from = typeof(string);
+            var to = typeof(int);
+
+            var p = Expression.Parameter(from, "p");
+
+            var body = Expression.Condition(
+                Expression.NotEqual(p, Expression.Constant(null, from)),
+                Expression.Convert(p, to, to.GetMethod(nameof(int.Parse), new[] { from })),
+                Expression.Constant(0));
+
+            var expr = Expression.Lambda<Func<string, int>>(body, p);
+
+            var compiled = expr.CompileFast();
+
+            Assert.AreEqual(10, compiled("10"));
+        }
+
+    }
+}
