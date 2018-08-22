777d5a4f8c4de5bc214c961753d51df5f24ff5ee
jkuehner <jochen.kuehner@kardex.com>
2018-08-22 22:26:27 +0200

    Tests for Issues #91 and #95

---
+++ test/FastExpressionCompiler.IssueTests/Issue91_Issue95_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using NUnit.Framework;
+
+namespace FastExpressionCompiler.IssueTests
+{
+    class Issue91_Issue95_Tests
+    {
+        delegate void ActionRef<T>(ref T a1);
+
+        [Test, Ignore("todo: fix")]
+        public void RefAssign()
+        {
+            var objRef = Expression.Parameter(typeof(double).MakeByRefType());
+            var lambda = Expression.Lambda<ActionRef<double>>(Expression.Assign(objRef, Expression.Add(objRef, Expression.Constant((double)3.0))), objRef);
+
+            var compiledA = lambda.Compile();
+            var exampleA = 5.0;
+            compiledA(ref exampleA);
+            Assert.AreEqual(8.0, exampleA);
+
+            var compiledB = lambda.CompileFast<ActionRef<double>>(true);
+            var exampleB = 5.0;
+            compiledB(ref exampleB);
+            Assert.AreEqual(8.0, exampleB);
+        }
+
+        [Test, Ignore("todo: fix")]
+        public void TestAddAssign()
+        {
+            var objRef = Expression.Parameter(typeof(double).MakeByRefType());
+            var lambda = Expression.Lambda<ActionRef<double>>(Expression.AddAssign(objRef, Expression.Constant((double)3.0)), objRef);
+
+            var compiledA = lambda.Compile();
+            var exampleA = 5.0;
+            compiledA(ref exampleA);
+            Assert.AreEqual(8.0, exampleA);
+
+            var compiledB = lambda.CompileFast<ActionRef<double>>(true);
+            var exampleB = 5.0;
+            compiledB(ref exampleB);
+            Assert.AreEqual(8.0, exampleB);
+        }
+    }
+}
