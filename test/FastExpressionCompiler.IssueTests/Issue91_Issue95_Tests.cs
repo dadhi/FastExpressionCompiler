eeb1153161377eda1baefcee80f7434f5e083e06
jkuehner <jochen.kuehner@kardex.com>
2018-08-24 20:02:06 +0200

    remove doubled test

--- test/FastExpressionCompiler.IssueTests/Issue91_Issue95_Tests.cs
+++ test/FastExpressionCompiler.IssueTests/Issue91_Issue95_Tests.cs
@@ -27,18 +27,6 @@ namespace FastExpressionCompiler.IssueTests
             Assert.AreEqual(8.0, exampleB);
         }
 
-        [Test, Ignore("needs fix")]
-        public void RefAssignExpInfo()
-        {
-            var objRef = FastExpressionCompiler.ExpressionInfo.Parameter(typeof(double).MakeByRefType());
-            var lambda = FastExpressionCompiler.ExpressionInfo.Lambda<ActionRef<double>>(FastExpressionCompiler.ExpressionInfo.Assign(objRef, FastExpressionCompiler.ExpressionInfo.Add(objRef, FastExpressionCompiler.ExpressionInfo.Constant((double)3.0))), objRef);
-
-            var compiledB = lambda.CompileFast(true);
-            var exampleB = 5.0;
-            compiledB(ref exampleB);
-            Assert.AreEqual(8.0, exampleB);
-        }
-
         [Test]
         public void NullComparisonTest()
         {
