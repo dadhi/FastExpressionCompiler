49d3f0ab1b8972985c22db825cf92b05b5dd8092
Maksim Volkau <max.volkov@gmail.com>
2018-08-30 22:21:39 +0300

    fix attempt 2

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 dotnet restore
 
-if test "$TRAVIS_OS_NAME" != "osx";
-then 
+# if test "$TRAVIS_OS_NAME" != "osx";
+# then 
 # todo: Until .Net Core 2 / 2.1 support
 # dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp1.1;
 # dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp1.1;
-fi
+# fi
 
 dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp2.0;
 dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp2.0;
