289d50ab9c6b4573baa3aee21733909438af46cb
Maksim Volkau <max.volkov@gmail.com>
2018-08-30 22:17:32 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if test "$TRAVIS_OS_NAME" != "osx";
 then 
 # todo: Until .Net Core 2 / 2.1 support
 # dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp1.1;
-dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp1.1;
+# dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp1.1;
 fi
 
 dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp2.0;
