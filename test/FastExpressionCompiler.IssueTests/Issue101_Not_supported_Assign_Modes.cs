3ce572c8f2471440e5ed71800ac9c72af618e24f
jkuehner <jochen.kuehner@kardex.com>
2018-08-31 00:01:37 +0200

    Try to work on Pre/Post Inc/dec

--- test/FastExpressionCompiler.IssueTests/Issue101_Not_supported_Assign_Modes.cs
+++ test/FastExpressionCompiler.IssueTests/Issue101_Not_supported_Assign_Modes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection;
 using NUnit.Framework;
 using static System.Linq.Expressions.Expression;
 
@@ -63,6 +64,99 @@ namespace FastExpressionCompiler.IssueTests
             var blockExpr =
                 Block(new[] { eVar },
                     PostDecrementAssign(eVar)
+                );
+
+            var lambda = Lambda<Func<int>>(blockExpr);
+            var fastCompiled = lambda.CompileFast(true);
+            var compiled = lambda.Compile();
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(compiled(), fastCompiled());
+        }
+
+        [Test]
+        public void PreIncShortIsSupported()
+        {
+            var eVar = Variable(typeof(short));
+            var blockExpr =
+                Block(new[] { eVar },
+                    PreIncrementAssign(eVar)
+                );
+
+            var lambda = Lambda<Func<short>>(blockExpr);
+            var fastCompiled = lambda.CompileFast(true);
+            var compiled = lambda.Compile();
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(compiled(), fastCompiled());
+        }
+
+        [Test]
+        public void PreInc3IsSupported()
+        {
+            var eVar = Variable(typeof(int));
+            var blockExpr =
+                Block(new[] {eVar},
+                    PreIncrementAssign(eVar),
+                    PreIncrementAssign(eVar),
+                    PreIncrementAssign(eVar)
+                );
+
+            var lambda = Lambda<Func<int>>(blockExpr);
+            var fastCompiled = lambda.CompileFast(true);
+            var compiled = lambda.Compile();
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(compiled(), fastCompiled());
+        }
+
+        [Test]
+        public void ComplexSupported()
+        {
+            int j = 0;
+            Action a = () => { j++; };
+            var eVar = Variable(typeof(int));
+            var pVar = Parameter(typeof(Action));
+            var blockExpr =
+                Block(new[] { eVar },
+                    Call(Constant(a), a.GetType().GetTypeInfo().GetDeclaredMethod("Invoke")),
+                    PreIncrementAssign(eVar)
+                    //PreIncrementAssign(eVar),
+                    //PreIncrementAssign(eVar)
+                );
+
+            var lambda = Lambda<Func<int>>(blockExpr);
+            var fastCompiled = lambda.CompileFast(true);
+            var compiled = lambda.Compile();
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(compiled(), fastCompiled());
+        }
+
+        [Test]
+        public void Complex2Supported()
+        {
+            int j = 0;
+            Func<int> a = () => { return j++; };
+            var eVar = Variable(typeof(int));
+            var blockExpr =
+                Block(new[] { eVar },
+                    Call(Constant(a), a.GetType().GetTypeInfo().GetDeclaredMethod("Invoke"))
+                );
+
+            var lambda = Lambda<Func<int>>(blockExpr);
+            var fastCompiled = lambda.CompileFast(true);
+            var compiled = lambda.Compile();
+            Assert.NotNull(fastCompiled);
+            Assert.AreEqual(compiled(), fastCompiled());
+        }
+
+        [Test]
+        public void Complex3Supported()
+        {
+            int j = 0;
+            Func<int> a = () => { return j++; };
+            var eVar = Variable(typeof(int));
+            var blockExpr =
+                Block(new[] { eVar },
+                    Call(Constant(a), a.GetType().GetTypeInfo().GetDeclaredMethod("Invoke")),
+                    PreIncrementAssign(eVar)
                 );
 
             var lambda = Lambda<Func<int>>(blockExpr);
