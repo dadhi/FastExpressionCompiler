14d214a29d60ed620a76f1af5c2a5efc7379cbd5
dadhi <max.volkov@gmail.com>
2019-01-25 17:23:49 +0000

    attempt to fix the builds on travis due no signing support on MacOS and no source-link support on Ubuntu

--- build.sh
+++ build.sh
@@ -4,15 +4,8 @@ set -euo pipefail
 
 dotnet restore
 
-# if test "$TRAVIS_OS_NAME" != "osx";
-# then 
-# todo: Until .Net Core 2 / 2.1 support
-# dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp1.1;
-# dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp1.1;
-# fi
+dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
+dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj   -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
 
-dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp2.0;
-dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj -c Release -f netcoreapp2.0;
-
-dotnet test test/FastExpressionCompiler.LightExpression.IssueTests/FastExpressionCompiler.LightExpression.IssueTests.csproj -c Release -f netcoreapp2.0;
-dotnet test test/FastExpressionCompiler.LightExpression.UnitTests/FastExpressionCompiler.LightExpression.UnitTests.csproj -c Release -f netcoreapp2.0;
+dotnet test test/FastExpressionCompiler.LightExpression.IssueTests/FastExpressionCompiler.LightExpression.IssueTests.csproj -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
+dotnet test test/FastExpressionCompiler.LightExpression.UnitTests/FastExpressionCompiler.LightExpression.UnitTests.csproj   -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
