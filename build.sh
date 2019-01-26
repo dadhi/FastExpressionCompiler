bdcddf40911c6bdf8d32eee5ee96ce3721963277
dadhi <max.volkov@gmail.com>
2019-01-26 09:45:33 +0300

    fixed: builds on Travis CI

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 dotnet restore
 
-dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
-dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj   -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
+dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
+dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj   -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
 
-dotnet test test/FastExpressionCompiler.LightExpression.IssueTests/FastExpressionCompiler.LightExpression.IssueTests.csproj -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
-dotnet test test/FastExpressionCompiler.LightExpression.UnitTests/FastExpressionCompiler.LightExpression.UnitTests.csproj   -c Release -f netcoreapp2.0 -p:Sign=false -p:SourceLink=false;
+dotnet test test/FastExpressionCompiler.LightExpression.IssueTests/FastExpressionCompiler.LightExpression.IssueTests.csproj -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
+dotnet test test/FastExpressionCompiler.LightExpression.UnitTests/FastExpressionCompiler.LightExpression.UnitTests.csproj   -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
