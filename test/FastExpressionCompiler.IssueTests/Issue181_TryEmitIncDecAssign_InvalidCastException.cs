555a10a617b3d1f95affd4f366890f2be2822beb
Steve Wilkes <steve@agileobjects.co.uk>
2019-05-03 09:07:31 +0100

    Test coverage for nested property pre-inc-assign

--- test/FastExpressionCompiler.IssueTests/Issue181_TryEmitIncDecAssign_InvalidCastException.cs
+++ test/FastExpressionCompiler.IssueTests/Issue181_TryEmitIncDecAssign_InvalidCastException.cs
@@ -10,8 +10,6 @@ using static System.Linq.Expressions.Expression;
 namespace FastExpressionCompiler.UnitTests
 #endif
 {
-    using System.Reflection.Emit;
-
     public class Issue181_TryEmitIncDecAssign_InvalidCastException
     {
         // originally seen in a Rezolver example, which I've tried to replicate as close as possible
@@ -32,31 +30,6 @@ namespace FastExpressionCompiler.UnitTests
         [Test]
         public void TryEmitIncDecAssign_Supports_PreIncrement_Property_Action()
         {
-            //var method = new DynamicMethod(
-            //    string.Empty,
-            //    typeof(void),
-            //    new[] { typeof(Issue181_TryEmitIncDecAssign_InvalidCastException) },
-            //    typeof(ExpressionCompiler),
-            //    skipVisibility: true);
-
-            //var il = method.GetILGenerator();
-
-            //var field = typeof(Issue181_TryEmitIncDecAssign_InvalidCastException).GetField("CounterField");
-
-            //il.Emit(OpCodes.Ldarg_0);
-            //il.Emit(OpCodes.Dup);
-            //il.Emit(OpCodes.Ldfld, field);
-            //il.Emit(OpCodes.Ldc_I4_1);
-            //il.Emit(OpCodes.Add);
-            //il.Emit(OpCodes.Stfld, field);
-            //il.Emit(OpCodes.Ret);
-
-            //var @delegate = method.CreateDelegate(typeof(Action<Issue181_TryEmitIncDecAssign_InvalidCastException>), null);
-
-            //var func = (Action<Issue181_TryEmitIncDecAssign_InvalidCastException>)@delegate;
-
-            //func.Invoke(this);
-
             var p = Parameter(typeof(Issue181_TryEmitIncDecAssign_InvalidCastException));
 
             var lambda = Lambda<Action<Issue181_TryEmitIncDecAssign_InvalidCastException>>(
@@ -71,6 +44,25 @@ namespace FastExpressionCompiler.UnitTests
             del.Invoke(this);
 
             Assert.AreEqual(expectedValue, CounterProperty);
+        }
+
+        [Test]
+        public void TryEmitIncDecAssign_Supports_PreIncrement_Nested_Property_Action()
+        {
+            var p = Parameter(typeof(Issue181_TryEmitIncDecAssign_InvalidCastException));
+
+            var lambda = Lambda<Action<Issue181_TryEmitIncDecAssign_InvalidCastException>>(
+                PreIncrementAssign(
+                    Property(Field(p, nameof(CounterObjField)), nameof(CounterProperty))
+                ),
+                p);
+
+            var del = lambda.CompileFast();
+
+            var expectedValue = CounterObjField.CounterProperty + 1;
+            del.Invoke(this);
+
+            Assert.AreEqual(expectedValue, CounterObjField.CounterProperty);
         }
 
         [Test]
