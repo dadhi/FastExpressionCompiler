40d3d1c9bad06bce3b38d3f80c2222a903d8b6c4
dadhi <max.volkov@gmail.com>
2019-12-12 20:02:38 +0000

    fixing build on TravisCI

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-dotnet restore -p:SourceLink=false
+dotnet restore -p:SourceLink=false;DevMode=false
 
-dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
-dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj   -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
+dotnet test test/FastExpressionCompiler.IssueTests/FastExpressionCompiler.IssueTests.csproj -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false;DevMode=false
+dotnet test test/FastExpressionCompiler.UnitTests/FastExpressionCompiler.UnitTests.csproj   -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false;DevMode=false
 
-dotnet test test/FastExpressionCompiler.LightExpression.IssueTests/FastExpressionCompiler.LightExpression.IssueTests.csproj -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
-dotnet test test/FastExpressionCompiler.LightExpression.UnitTests/FastExpressionCompiler.LightExpression.UnitTests.csproj   -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false
+dotnet test test/FastExpressionCompiler.LightExpression.IssueTests/FastExpressionCompiler.LightExpression.IssueTests.csproj -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false;DevMode=false
+dotnet test test/FastExpressionCompiler.LightExpression.UnitTests/FastExpressionCompiler.LightExpression.UnitTests.csproj   -c:Release -f:netcoreapp2.0 -p:Sign=false;SourceLink=false;DevMode=false
