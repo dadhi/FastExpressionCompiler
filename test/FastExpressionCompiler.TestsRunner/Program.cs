02f1d3d8898f8aec0046d550b406365cf631bf14
maximv <maximv@cynet.com>
2020-07-14 23:24:26 +0300

    add test for #251

--- test/FastExpressionCompiler.TestsRunner/Program.cs
+++ test/FastExpressionCompiler.TestsRunner/Program.cs
@@ -11,6 +11,8 @@ namespace FastExpressionCompiler.UnitTests
         {
             RunAllTests();
 
+            // new FastExpressionCompiler.LightExpression.IssueTests.Issue251_Bad_code_gen_for_byRef_parameters().Run();
+
             // todo: @incomplete, try other cases
             // new FastExpressionCompiler.LightExpression.IssueTests
             //     .Issue248_Calling_method_with_in_out_parameters_in_expression_lead_to_NullReferenceException_on_calling_site().Run();
