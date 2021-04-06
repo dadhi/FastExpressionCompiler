a926175cb54120f8b7863e6d3526e9f3d04c129d
dadhi <max.volkov@gmail.com>
2021-04-06 23:06:29 +0300

    releasing v3.0.4

--- BuildScripts/NuGetPublish.bat
+++ BuildScripts/NuGetPublish.bat
@@ -4,7 +4,7 @@ set PACKAGES=..\.dist
 set SOURCE=https://api.nuget.org/v3/index.json
 set /p APIKEY=<"..\..\ApiKey.txt"
 
-set PKGVER=3.0.3
+set PKGVER=3.0.4
 
 dotnet nuget push "%PACKAGES%\FastExpressionCompiler.%PKGVER%.nupkg" -k %APIKEY% -s %SOURCE%
 dotnet nuget push "%PACKAGES%\FastExpressionCompiler.src.%PKGVER%.nupkg" -k %APIKEY% -s %SOURCE%
