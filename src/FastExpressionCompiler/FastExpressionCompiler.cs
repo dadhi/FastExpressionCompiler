07acff238137afed2f0b8c3d7df9e1b15dc60989
dadhi <max.volkov@gmail.com>
2023-01-24 09:58:29 +0100

    fix doc comments for CompilerFlags

--- src/FastExpressionCompiler/FastExpressionCompiler.cs
+++ src/FastExpressionCompiler/FastExpressionCompiler.cs
@@ -53,17 +53,17 @@ namespace FastExpressionCompiler
     using System.Diagnostics;
     using static System.Environment;
 
-    /// <summary>The options for the compiler</summary>
+    /// <summary>The flags for the compiler</summary>
     [Flags]
     public enum CompilerFlags : byte
     {
-        /// <summary>The default options: Invocation lambda is inlined, no debug info</summary>
+        /// <summary>The default flags: Invocation lambda is inlined, no debug info</summary>
         Default = 0,
         /// <summary>Prevents the inlining of the lambda in the Invocation expression to optimize for the multiple same lambda compiled once</summary>
         NoInvocationLambdaInlining = 1,
         /// <summary>Adds the Expression, ExpressionString, and CSharpString to the delegate closure for the debugging inspection</summary>
         EnableDelegateDebugInfo = 1 << 1,
-        /// <summary>When the flag set then instead of the returning `null` the specific exception</summary>
+        /// <summary>When the flag is set then instead of the returning `null` the specific exception is thrown</summary>
         ThrowOnNotSupportedExpression = 1 << 2
     }
 
