773ad2db311bf76b006d96bfb86bb48ac5e2a913
Standa Lukeš <exyi@outlook.cz>
2023-10-01 17:43:14 +0200

    ToCSharpString: fix TypeEqual expression
    Output this expression same as before, if
    TypeIs and TypeEqual are equivalent (type cannot have derived classes);
    otherwise as a.GetType() == typeof(B)

--- test/FastExpressionCompiler.UnitTests/ToCSharpStringTests.cs
+++ test/FastExpressionCompiler.UnitTests/ToCSharpStringTests.cs
@@ -31,6 +31,21 @@ namespace FastExpressionCompiler.UnitTests
             Assert.AreEqual(typeof(A<string>), f());
         }
 
+        [Test]
+        public void Outputs_type_equals()
+        {
+            var p = Parameter(typeof(object), "p");
+            var eSealed = TypeEqual(p, typeof(string));
+            var eStruct = TypeEqual(p, typeof(int));
+            var eArray = TypeEqual(p, typeof(object[]));
+            var eOpen = TypeEqual(p, typeof(System.Collections.Generic.List<string>));
+
+            Assert.AreEqual("(p is string);", eSealed.ToCSharpString());
+            Assert.AreEqual("(p is int);", eStruct.ToCSharpString());
+            Assert.AreEqual("(p.GetType() == typeof(object[]));", eArray.ToCSharpString());
+            Assert.AreEqual("(p.GetType() == typeof(List<string>));", eOpen.ToCSharpString());
+        }
+
         class A<X> {}
     }
 }
