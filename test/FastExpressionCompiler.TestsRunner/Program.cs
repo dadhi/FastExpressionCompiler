ae88c0409fbb513013e9d1b2475a66259014d3e5
dadhi <max.volkov@gmail.com>
2023-12-20 18:51:48 +0100

    cleanup debug lefovers

--- test/FastExpressionCompiler.TestsRunner/Program.cs
+++ test/FastExpressionCompiler.TestsRunner/Program.cs
@@ -12,30 +12,9 @@ namespace FastExpressionCompiler.UnitTests
         public static void Main()
         {
             // new LightExpression.IssueTests.Issue346_Is_it_possible_to_implement_ref_local_variables().Run();
-            // new EmitHacksTest().Run();
             // new LightExpression.IssueTests.Issue365_Working_with_ref_return_values().Run();
-            // new Issue353_NullReferenceException_when_calling_CompileFast_results().Run();
-            // new Issue101_Not_supported_Assign_Modes().Run();
-            // new Issue170_Serializer_Person_Ref().Run();
-            // new LightExpression.IssueTests.Issue55_CompileFast_crash_with_ref_parameter().Run();
-            // new LightExpression.IssueTests.Issue352_xxxAssign_does_not_work_with_MemberAccess().Run();
-            // new BlockTests().Run();
-            // new LightExpression.UnitTests.NestedLambdasSharedToExpressionCodeStringTest().Run();
-            // new Issue352_xxxAssign_does_not_work_with_MemberAccess().Run();
-            // new UnaryExpressionTests().Run();
-            // new NestedLambdaTests().Run();
-            // new LightExpression.UnitTests.BlockTests().Run();
-            new Issue374_CompileFast_doesnot_work_with_HasFlag().Run();
-            // new LightExpression.IssueTests.Issue347_InvalidProgramException_on_compiling_an_expression_that_returns_a_record_which_implements_IList().Run();
-            // new Issue281_Index_Out_of_Range().Run();
-            // new ConvertOperatorsTests().Run();
-            // new LightExpression.UnitTests.ConvertOperatorsTests().Run();
 
             RunAllTests();
-
-            // Console.WriteLine("Local variable usage:");
-            // foreach (var t in FastExpressionCompiler.ILGeneratorHacks.LocalVarUsage.OrderByDescending(x => x.Value))
-            //     Console.WriteLine($"{t.Key.Name} -> {t.Value}");
         }
 
         public static void RunAllTests()
