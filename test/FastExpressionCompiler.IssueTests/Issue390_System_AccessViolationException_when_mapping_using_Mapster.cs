5d43c86fd3743267465a46efbfc05a5a068437ea
dadhi <max.volkov@gmail.com>
2024-02-16 12:46:14 +0100

    first minimal extracted test @wip param in root lambda used by nested lambdas should be predefined for the ToExpression

--- test/FastExpressionCompiler.IssueTests/Issue390_System_AccessViolationException_when_mapping_using_Mapster.cs
+++ test/FastExpressionCompiler.IssueTests/Issue390_System_AccessViolationException_when_mapping_using_Mapster.cs
@@ -17,10 +17,140 @@ public class Issue390_System_AccessViolationException_when_mapping_using_Mapster
 {
     public int Run()
     {
+        Test_extracted_small_mapping_code();
         Test_extracted_mapping_code();
         Test_mapping();
 
-        return 2;
+        return 3;
+    }
+
+    [Test]
+    public void Test_extracted_small_mapping_code()
+    {
+        var p = new ParameterExpression[7]; // the parameter expressions
+        var e = new Expression[48]; // the unique expressions
+        var l = new LabelTarget[1]; // the labels
+        p[1]=Parameter(typeof(AuthResultDto));
+        var expr = Lambda<Func<AuthResultDto, Token>>(
+        e[0]=Block(
+            typeof(Token),
+            new[] {
+            p[0]=Parameter(typeof(MapContextScope), "scope")
+            },
+            e[7]=MakeBinary(ExpressionType.Assign,
+                p[0 // (MapContextScope scope)
+                    ],
+                e[8]=New( // 0 args
+                    typeof(MapContextScope).GetTypeInfo().DeclaredConstructors.AsArray()[0], new Expression[0])), 
+            e[9]=TryCatchFinally(
+                e[10]=Block(
+                    typeof(void),
+                    new[] {
+                    p[2]=Parameter(typeof(object), "cache"),
+                    p[3]=Parameter(typeof(Dictionary<ReferenceTuple, object>), "references"),
+                    p[4]=Parameter(typeof(ReferenceTuple), "key"),
+                    p[5]=Parameter(typeof(Token), "result")
+                    },
+                    e[11]=MakeBinary(ExpressionType.Assign,
+                        p[3 // (Dictionary<ReferenceTuple, object> references)
+                            ],
+                        e[12]=Property(
+                            e[13]=Property(
+                                p[0 // (MapContextScope scope)
+                                    ],
+                                typeof(MapContextScope).GetTypeInfo().GetDeclaredProperty("Context")),
+                            typeof(MapContext).GetTypeInfo().GetDeclaredProperty("References"))), 
+                    e[14]=MakeBinary(ExpressionType.Assign,
+                        p[4 // ([struct] ReferenceTuple key)
+                            ],
+                        e[15]=New( // 2 args
+                            typeof(ReferenceTuple).GetTypeInfo().DeclaredConstructors.AsArray()[0],
+                            p[1]=Parameter(typeof(AuthResultDto)), 
+                            e[16]=Constant(typeof(Token)))), 
+                    e[21]=MakeBinary(ExpressionType.Assign,
+                        p[5 // (Token result)
+                            ],
+                        e[22]=New( // 0 args
+                            typeof(Token).GetTypeInfo().DeclaredConstructors.AsArray()[0], new Expression[0])), 
+                    e[23]=MakeBinary(ExpressionType.Assign,
+                        e[24]=MakeIndex(
+                            p[3 // (Dictionary<ReferenceTuple, object> references)
+                                ], 
+                            typeof(Dictionary<ReferenceTuple, object>).GetTypeInfo().GetDeclaredProperty("Item"), new Expression[] {
+                            p[4 // ([struct] ReferenceTuple key)
+                                ]}),
+                        e[25]=Convert(
+                            p[5 // (Token result)
+                                ],
+                            typeof(object))), 
+                    e[26]=Block(
+                        typeof(DateTime),
+                        new ParameterExpression[0], 
+                        e[27]=MakeBinary(ExpressionType.Assign,
+                            e[28]=Property(
+                                p[5 // (Token result)
+                                    ],
+                                typeof(Token).GetTypeInfo().GetDeclaredProperty("RefreshTokenExpirationDate")),
+                            e[29]=Invoke(
+                                e[30]=Lambda<Func<DateTime?, DateTime>>(
+                                    e[31]=Condition(
+                                        e[32]=MakeBinary(ExpressionType.Equal,
+                                            p[6]=Parameter(typeof(DateTime?)),
+                                            e[33]=Constant(null, typeof(DateTime?)),
+                                            liftToNull: false,
+                                            typeof(DateTime).GetMethods().Single(x => !x.IsGenericMethod && x.Name == "op_Equality" && x.GetParameters().Select(y => y.ParameterType).SequenceEqual(new[] { typeof(DateTime), typeof(DateTime) }))),
+                                        e[34]=Constant(DateTime.Parse("1/1/0001 12:00:00 AM")),
+                                        e[35]=Convert(
+                                            p[6 // ([struct] DateTime? datetime__9799115)
+                                                ],
+                                            typeof(DateTime)),
+                                        typeof(DateTime)),
+                                    p[6 // ([struct] DateTime? datetime__9799115)
+                                        ]),
+                                e[36]=Condition(
+                                    e[37]=MakeBinary(ExpressionType.Equal,
+                                        e[38]=Property(
+                                            p[1],
+                                            typeof(AuthResultDto).GetTypeInfo().GetDeclaredProperty("RefreshToken")),
+                                        e[39]=Constant(null, typeof(RefreshTokenDto))),
+                                    e[40]=Constant(null, typeof(DateTime?)),
+                                    e[41]=Convert(
+                                        e[42]=Property(
+                                            e[43]=Property(
+                                                e[38 // MemberAccess of RefreshTokenDto
+                                                    ],
+                                                typeof(RefreshTokenDto).GetTypeInfo().GetDeclaredProperty("ExpirationDate")),
+                                            typeof(DateTimeOffset).GetTypeInfo().GetDeclaredProperty("LocalDateTime")),
+                                        typeof(DateTime?)),
+                                    typeof(DateTime?))))), 
+                    e[44]=MakeGoto(GotoExpressionKind.Return,
+                        l[0]=Label(typeof(Token)),
+                        p[5 // (Token result)
+                            ],
+                        typeof(void))),
+                e[45]=Call(
+                    p[0 // (MapContextScope scope)
+                        ], 
+                    typeof(MapContextScope).GetMethods().Single(x => !x.IsGenericMethod && x.Name == "Dispose" && x.GetParameters().Length == 0)),new CatchBlock[0]), 
+            e[46]=Label(l[0 // (issue390_system_accessviolationexception_when_mapping_using_mapster_token__41962596)
+                ],
+                e[47]=Constant(null, typeof(Token)))),
+        p[1]);
+
+        expr.PrintCSharp();
+
+        var auth = new AuthResultDto() { RefreshToken = new() };
+
+        var fs = expr.CompileSys();
+        fs.PrintIL();
+        var token = fs(auth);
+        Assert.AreEqual(auth.RefreshToken.ExpirationDate.LocalDateTime, token.RefreshTokenExpirationDate);
+
+        // var ff = expr.CompileFast(true); // todo: @fixme
+        var ff = expr.CompileFast(true, flags: CompilerFlags.NoInvocationLambdaInlining);
+        ff.PrintIL();
+        token = ff(auth);
+        Assert.AreEqual(auth.RefreshToken.ExpirationDate.LocalDateTime, token.RefreshTokenExpirationDate);
     }
 
     [Test]
